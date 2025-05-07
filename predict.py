eab1daeafe392294a4291e5d48c7077c23e7af6f
Vathsala <vathsala.sachar@gmail.com>
2025-05-07 15:09:09 +0100

    added logging

--- predict.py
+++ predict.py
@@ -7,6 +7,9 @@ import onnx
 import onnxruntime as ort
 import load_model
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 def preprocess_image(image_path: Path):
     preprocess = v2.Compose([
@@ -30,20 +33,22 @@ def predict_class(image_path: Path):
     # get preprocessed input image
     input_img = preprocess_image(image_path)
 
-    # get class labels
-    with open(load_model.IMAGENET_CLASS_PATH, "r") as fp:
-        class_labels = [label.strip() for label in fp.readlines()]
-
-    # load model and run checker ## not sure if step is needed
+    # load model and run checker
     res18_model = onnx.load(load_model.RESNET18_MODEL_PATH)
     onnx.checker.check_model(res18_model)
 
     # start onnx runtime session
     ort_session = ort.InferenceSession(load_model.RESNET18_MODEL_PATH)
-
     output = ort_session.run(None, {'data': input_img.numpy()})
     output = np.squeeze(output[0])
+    class_index = output.argmax(0)
 
-    # log this
-    print(class_labels[output.argmax(0)])
-    return output.argmax(0)
+    # get class labels
+    with open(load_model.IMAGENET_CLASS_PATH, "r") as fp:
+        class_labels = [label.strip() for label in fp.readlines()]
+    # logging the class label for reference
+    logger.info(
+        f"Class label of input image: {class_labels[class_index]}")
+
+    # Return class index
+    return class_index
