2ab93a55f8c7db4b616ea71d54070295c9ea2d7b
Vathsala <vathsala.sachar@gmail.com>
2025-05-08 08:33:56 +0100

    only accept image content in the file upload

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def model_inference(file: UploadFile):
     '''
     Returns the predicted class index when given an image
     '''
-    # save the file locally
+    # check if image and return error message if not
+    if not "image" in file.content_type:
+        return {"file_type": file.content_type, "error_message": "Not an image. Please upload image file."}
+
+    # save the image locally
     img_file_path = Path(f"./{file.filename}")
     img = Image.open(file.file)
     img.save(img_file_path)
